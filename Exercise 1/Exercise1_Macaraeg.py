ac0a479468185588c9e4d0498588d4a28d0b260b
John Austin S. Macaraeg <jsmacaraeg1@up.edu.ph>
2024-02-27 16:17:44 +0800

    Exercise 1

---
+++ Exercise 1/Exercise1_Macaraeg.py
@@ -0,0 +1,8 @@
+"""
+GE 120: Introduction to Programming
+Multi-line comment
+exercise1
+austin macaraeg
+"""
+
+
---
+++ Lecture2_Macaraeg.py
@@ -0,0 +1,46 @@
+print("Kumusta Mundo")
+print(type("Kumusta Mundo"))
+print(type(str(1)))
+# Comments = ctrl + / (#)
+# Concatenation - adding two strings together
+print("Wazzup" + " GE 120")\
+
+"""
+This is a multi line comment
+hello
+hello
+hello
+"""
+
+# this is a single line comment
+
+print("This is a comment") # this is an inline comment
+
+# DIFFERENT DATA TYPES
+print(type("STRING")) #str
+print(type(54)) #int
+print(type(3.1415265358)) #float
+print(type(1+4j)) #complex
+
+# CONVERTING DATA TYPE
+print(type(float(54))) #convert int to float
+
+# VARIABLES
+x = 2
+y = 13
+
+# ARITHMETIC OPERATIONS
+print("Sum of X and Y: ", x + y) #addition
+print("Difference of X and Y: ", x - y) #subtraction
+print("Product of X and Y: ", x * y) #multiplication
+print("True Quotient of X and Y: ", x / y)
+print("Floor Division of X and Y: ", x // y)
+print("Exponent of X and Y: ", x ** y)
+print("Remainder of X and Y: ", x % y)
+print("Abs value of X and Y: ", abs(y))
+print("Float value of X and Y: ", float(y))
+print("Complex value of X and Y: ", complex(y))
+print("Divmod of Y over X: ", divmod(y,x))
+
+# LISTS
+dog = ["golden retriever", "husky", "aspin" "k9"]
